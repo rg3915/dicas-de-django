1c8d196a4d84608592ef7f264af189f4d3116a4f
rg3915 <rg3915@yahoo.com.br>
2020-07-09 13:57:14 -0300

    Templates

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', v.index, name='index'),
+    path('persons/', v.person_list, name='person_list'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     template_name = 'index.html'
     return render(request, template_name)
+
+
+def person_list(request):
+    template_name = 'core/person_list.html'
+    return render(request, template_name)
