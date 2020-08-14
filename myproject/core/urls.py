3b074db8469d792e85b798994b677f9b820df4f3
rg3915 <rg3915@yahoo.com.br>
2020-08-14 02:24:45 -0300

    Template article_list.

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -8,4 +8,5 @@ app_name = 'core'
 urlpatterns = [
     path('', v.index, name='index'),
     path('persons/', v.person_list, name='person_list'),
+    path('articles/', v.article_list, name='article_list'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Article
 
 
 def index(request):
@@ -9,3 +10,10 @@ def index(request):
 def person_list(request):
     template_name = 'core/person_list.html'
     return render(request, template_name)
+
+
+def article_list(request):
+    template_name = 'core/article_list.html'
+    object_list = Article.objects.all()
+    context = {'object_list': object_list}
+    return render(request, template_name, context)
