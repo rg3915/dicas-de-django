7e6c1bc69002a200e15f3add8cbec508c3755abd
rg3915 <regis42santos@gmail.com>
2022-11-25 15:47:08 -0300

    Logout. close #107

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,8 @@
 # accounts/urls.py
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),  # noqa E501
+    path('logout/', LogoutView.as_view(), name='logout'),  # noqa E501
 ]
