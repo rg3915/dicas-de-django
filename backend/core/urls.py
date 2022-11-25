36efcf1749c49ef041ac1c4bf499ef3ef071ed08
rg3915 <regis42santos@gmail.com>
2022-11-25 15:27:34 -0300

    Dashboard com Django e Tailwind CSS. close #104

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', v.index, name='index'),  # noqa E501
+    path('dashboard/', v.dashboard, name='dashboard'),  # noqa E501
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     template_name = 'index.html'
     return render(request, template_name)
+
+
+def dashboard(request):
+    template_name = 'dashboard.html'
+    return render(request, template_name)
