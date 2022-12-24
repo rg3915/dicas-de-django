307a89f949cf63b05b70192ba9068551fd13fdf8
rg3915 <regis42santos@gmail.com>
2022-12-24 06:07:30 -0300

    ManyToMany. close #112

--- backend/bookstore/admin.py
+++ backend/bookstore/admin.py
@@ -1,7 +1,7 @@
 # bookstore/admin.py
 from django.contrib import admin
 
-from .models import Customer, Ordered, Sale
+from .models import Author, Book, Customer, Ordered, Publisher, Sale, Store
 
 
 @admin.register(Customer)
@@ -28,3 +28,36 @@ class SaleAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'paid', 'date_paid', 'method', 'deadline')
     list_filter = ('paid', 'method')
     date_hierarchy = 'created'
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('first_name', 'last_name')
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'isbn',
+        '__str__',
+        'rating',
+        'price',
+        'stock_min',
+        'stock',
+        'publisher',
+    )
+    list_display_links = ('__str__',)
+    search_fields = ('isbn', 'title')
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('name',)
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('name',)
--- backend/bookstore/models.py
+++ backend/bookstore/models.py
@@ -89,3 +89,89 @@ class Sale(models.Model):
             return f'{str(self.pk).zfill(3)}-{self.ordered}'
 
         return f'{str(self.pk).zfill(3)}'
+
+
+class Author(models.Model):
+    first_name = models.CharField('nome', max_length=100)
+    last_name = models.CharField('sobrenome', max_length=255, null=True, blank=True)  # noqa E501
+
+    class Meta:
+        ordering = ('first_name',)
+        verbose_name = 'autor'
+        verbose_name_plural = 'autores'
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name or ""}'.strip()
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=13, unique=True)
+    title = models.CharField('título', max_length=255)
+    rating = models.DecimalField('pontuação', max_digits=5, decimal_places=2, default=5)
+    authors = models.ManyToManyField(
+        Author,
+        verbose_name='autores',
+        blank=True
+    )
+    price = models.DecimalField('preço', max_digits=5, decimal_places=2)
+    stock_min = models.PositiveSmallIntegerField(default=0)
+    stock = models.PositiveSmallIntegerField(default=0)
+    publisher = models.ForeignKey(
+        'Publisher',
+        on_delete=models.SET_NULL,
+        verbose_name='editora',
+        related_name='books',
+        null=True,
+        blank=True
+    )
+    created = models.DateTimeField(
+        'criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified = models.DateTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True
+    )
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'livro'
+        verbose_name_plural = 'livros'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Store(models.Model):
+    name = models.CharField('nome', max_length=255)
+    books = models.ManyToManyField(
+        Book,
+        verbose_name='livros',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'loja'
+        verbose_name_plural = 'lojas'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Publisher(models.Model):
+    name = models.CharField('nome', max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'editora'
+        verbose_name_plural = 'editoras'
+
+    def __str__(self):
+        return f'{self.name}'
